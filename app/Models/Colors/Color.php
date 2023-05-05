af5492c94490986393812088a8918ae4727538ce
Aziz Ismailov <azizismailov872872@gmail.com>
2023-05-05 12:58:03 +0600

    Создал контроллеры и модели для сущностей fabrics, colors

---
+++ app/Models/Colors/Color.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Colors;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Color extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
---
+++ app/Models/Fabrics/Fabric.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Fabrics;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fabric extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
